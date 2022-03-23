553b3553d10113d8c9620fe41fab8e0f8b2a2326
JCas03 <03juan.c.castorena@gmail.com>
2022-03-23 16:10:26 -0500

    a a

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa4ccnklyI9a8zE55d7',
+        issuer: 'https://dev-2815983.okta.com/oauth2/default',
+        redirectUri: 'https://az-frt.azurewebsites.net/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  luv2shopApiUrl: "https://bkend.azurewebsites.net/api",
+  stripePublishableKey: "<<ADD-YOUR-STRIPE-PUBLISHABLE-KEY>>"
+};
